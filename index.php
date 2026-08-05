c20049062f5bd70c11f50ac9782cf2b8a73810f0
arunsah.phelix@gmail.com <your_email@example.com>
2026-08-06 00:14:37 +0530

    first

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ render_nav('home');
                 <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/>
               </svg>
             </div>
-            <p>Your photo here</p>
+            <img src="photo/2.jpeg" alt="Your Name">
           </div>
           <!-- Replace with: <img src="assets/photo.jpg" alt="Your Name"> -->
         </div>
